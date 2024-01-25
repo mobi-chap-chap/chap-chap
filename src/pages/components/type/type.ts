8f73660d578aa9f0425231a51f4ffaabf76939ff
Ahnjuhyeon <134281116+Ahnjuhyeon@users.noreply.github.com>
2024-01-25 22:10:16 +0900

    🎁 style : recip-scrap-page publishing (#19)

---
+++ src/pages/components/type/type.ts
@@ -0,0 +1,7 @@
+//one-recipe.tsx type
+export type OneRecipeProps = {
+  recipeImg: string;
+  recipeType: string;
+  recipeKal: number;
+  recipeTitle: string;
+};
--- src/pages/scrap/scraps.tsx
+++ src/pages/scrap/scraps.tsx
@@ -1,10 +1,14 @@
+import NoScrap from "./components/no-scrap";
 import ScrapList from "./components/scrap-list";
+import ScrapRecipe from "./components/scrap.recipes";
 
 const ScrapPage = () => {
   return (
-    <>
+    <div className="w-screen h-screen flex flex-col items-center">
       <ScrapList />
-    </>
+      {/* <NoScrap /> */}
+      <ScrapRecipe />
+    </div>
   );
 };
 export default ScrapPage;
